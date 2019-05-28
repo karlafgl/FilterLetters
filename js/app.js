aad67159945de44a4ababce1e427122ce2cc46f8
Karlafgl <karlafgl@gmail.com>
2019-05-28 11:45:01 -0600

    adding funcionality to app.js to show the lyricis on the IU

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import {formularioBuscar, divBuscar, divMensajes, divResultado} from './interfaz.js'
 import { API } from './api.js'
 
+console.log(API)
 formularioBuscar.addEventListener('submit',(e)=> {
     e.preventDefault()
     
@@ -17,9 +18,19 @@ formularioBuscar.addEventListener('submit',(e)=> {
     } else {
         const api = new API(artista, cancion)
         api.consultarApi().then(data => {
-            console.log(data)
+            if(data.respuesta.lyrics){
+                const letra = data.respuesta.lyrics
+                divResultado.textContent = letra
+            } else {
+                divMensajes.innerHTML = 'The Song does not exist, try again'
+                divMensajes.classList.add('error')
+                setTimeout(() => {
+                    divMensajes.innerHTML = '';
+                    divMensajes.classList.remove('error')
+                    divMensajes.reset()
+                },3000)
+            }
         })
-
     }
 
 })
